34e07bd6e717ec969df484c7bf0937444e90b1ff
dida0423 <didak0423@gmail.com>
2026-04-21 13:11:18 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/server-options.html#server-proxy
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const openAiKey = env.OPENAI_API_KEY
+  const openAiKey = env.VITE_OPENAI_API_KEY
 
   return {
     plugins: [react()],
